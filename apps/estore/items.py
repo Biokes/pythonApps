d30af24b381e4be47da3e4f4455528e42237f1f4
Biokes <106444765+Biokes@users.noreply.github.com>
2024-02-19 09:48:35 +0100

    my codes

---
+++ apps/estore/items.py
@@ -0,0 +1,8 @@
+from apps.estore.product import Product
+
+
+class Items:
+
+    def __init__(self, product: Product, product_quantity: int):
+        self.product = product
+        self.product_quantity = product_quantity
