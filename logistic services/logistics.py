4babf22742586fc83d75128ad083a6ad537a7cc8
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-07 08:19:41 +0100

    my cides

--- logistic services/logistics.py
+++ logistic services/logistics.py
@@ -4,7 +4,7 @@ from apps.class_examples.invalidnumbererror import InvalidNumberError
 class Logistics:
     @staticmethod
     def calculateWage(deliveries: int):
-        if deliveries < 0:
+        if 0 > deliveries or deliveries > 100:
             raise InvalidNumberError
         return Logistics.__deliveriesRate(deliveries) + 5000
 
@@ -14,3 +14,7 @@ class Logistics:
             return number_of_deliveries * 160
         if 60 > number_of_deliveries >= 50:
             return number_of_deliveries * 200
+        if 70 > number_of_deliveries >= 60:
+            return number_of_deliveries * 250
+        if number_of_deliveries >= 70:
+            return number_of_deliveries * 500
--- logistic services/testlogistics.py
+++ logistic services/testlogistics.py
@@ -1,3 +1,6 @@
+import pytest
+
+from apps.class_examples.invalidnumbererror import InvalidNumberError
 from logistics import Logistics
 
 
@@ -8,4 +11,11 @@ class TestDispatchCalculator:
         assert Logistics.calculateWage(0) == 5000
         assert 15000 == Logistics.calculateWage(50)
         assert 16000 == Logistics.calculateWage(55)
-        # assert
+        assert Logistics.calculateWage(61) == 20250
+        assert Logistics.calculateWage(70) == 40000
+
+    def test_invalidDeliveries_raiseError(self):
+        with pytest.raises(InvalidNumberError):
+            Logistics.calculateWage(-1)
+        with pytest.raises(InvalidNumberError):
+            Logistics.calculateWage(1000)
