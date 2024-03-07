0788bb8a4d36934313466dcfe27a9a5b1707d4d8
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-07 08:03:17 +0100

    my cides

--- logistic services/logistics.py
+++ logistic services/logistics.py
@@ -4,10 +4,13 @@ from apps.class_examples.invalidnumbererror import InvalidNumberError
 class Logistics:
     @staticmethod
     def calculateWage(deliveries: int):
-        if deliveries < 1:
+        if deliveries < 0:
             raise InvalidNumberError
         return Logistics.__deliveriesRate(deliveries) + 5000
 
     @staticmethod
     def __deliveriesRate(number_of_deliveries: int):
-        return number_of_deliveries * 160
+        if number_of_deliveries < 50:
+            return number_of_deliveries * 160
+        if 60 > number_of_deliveries >= 50:
+            return number_of_deliveries * 200
--- logistic services/testlogistics.py
+++ logistic services/testlogistics.py
@@ -4,5 +4,8 @@ from logistics import Logistics
 class TestDispatchCalculator:
 
     def test_deliveriesAmountOIsCalculated(self):
-        assert 13000 == Logistics.calculateWage(50)
         assert Logistics.calculateWage(1) == 5160
+        assert Logistics.calculateWage(0) == 5000
+        assert 15000 == Logistics.calculateWage(50)
+        assert 16000 == Logistics.calculateWage(55)
+        # assert
