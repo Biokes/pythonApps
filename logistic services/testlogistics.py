490aae75fb1fc65dbe07d97616b7c6107002d87f
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-07 07:10:16 +0100

    my cides

--- logistic services/testlogistics.py
+++ logistic services/testlogistics.py
@@ -4,5 +4,5 @@ from logistics import Logistics
 class TestDispatchCalculator:
 
     def test_deliveriesAmountOIsCalculated(self):
-        logistics: Logistics = Logistics()
         assert 13000 == Logistics.calculateWage(50)
+        assert Logistics.calculateWage(1) == 5160
