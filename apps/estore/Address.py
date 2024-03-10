f31fe2abb0809a5aef9f1c4983ef1d3ff1ba39ab
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-10 07:24:17 +0100

    my cides

--- apps/estore/Address.py
+++ apps/estore/Address.py
@@ -1,8 +1,8 @@
 class Address:
 
-    def __init__(self, city: str, country: str, houseNumber: str, street: str, state: str):
+    def __init__(self, city: str, country: str, house_number: str, street: str, state: str):
         self.state = state
         self.street = street
-        self.houseNumber = houseNumber
+        self.house_number = house_number
         self.country = country
         self.city = city
--- bankApp/bankapp.py
+++ bankApp/bankapp.py
@@ -13,6 +13,9 @@ class BankApp:
         self.root = display.Tk()
         self.root.withdraw()
 
+    def __init__(self):
+        self.name = "name"
+
     def create_account(self):
         try:
             full_name = simpledialog.askstring("Create Account Menu", "Enter your full name")
@@ -184,145 +187,6 @@ welcome to Mavericks Bank🤭🤭🤭
             case _:
                 messagebox.showwarning("Error", "Wrong input\nselect form 1-7")
                 self.main_menu()
-
-#     def landing_page(self):
-#         choice = simpledialog.askstring("Welcome Message",
-#                                         "welcome To mavericks bank.\n1. create Account\n2. Exit App\nEnter your "
-#                                         "choice:  ")
-#         match choice:
-#             case "1":
-#                 self.create_account()
-#             case "2":
-#                 sys.exit()
-#             case _:
-#                 self.landing_page()
-#
-#     def home_page(self):
-#         try:
-#             option = self.full_display_menu()
-#             self.full_display(option)
-#         except KeyboardInterrupt as error:
-#             print(f"{error}\nidiot u wan break code abi😒😒😒😒😒😒.")
-#         except Exception:
-#             print("idiot u wan break code abi😒😒😒😒😒😒.")
-#         finally:
-#             self.home_page()
-#
-#
-#     def full_display_menu(self):
-#         return option
-#
-#
-#
-#     def case_two(self):
-#
-#         finally:
-#             self.full_display(self.full_display_menu())
-#
-#     def case_three(self):
-#         try:
-#
-#         finally:
-#             self.full_display(self.full_display_menu())
-#
-#     def case_four(self):
-#         try:
-#             print("\n\n<<<<<<<Transfer Menu>>>>>>>>>")
-#             sender = int(input("Enter sender's account Number: "))
-#             receiver = int(input("Enter the receiver's account: "))
-#             pin = input("Enter your pin: ")
-#             amount = float(input("Enter amount: "))
-#             print(self.bank.transfer(sender, amount, receiver, pin))
-#         except InvalidPinError as error:
-#             print(error)
-#             self.full_display(self.full_display_menu())
-#         except InvalidAccountNumberError as error:
-#             print(error)
-#             self.full_display(self.full_display_menu())
-#         except InvalidAmountError as error:
-#             print(error)
-#             self.full_display(self.full_display_menu())
-#         except AccountNotFoundError as error:
-#             print(error)
-#             self.full_display(self.full_display_menu())
-#         finally:
-#             self.full_display(self.full_display_menu())
-#
-#     def case_five(self):
-#         try:
-#             print("\n\n<<<<<<<<<<Check Balance Menu>>>>>>>>>\n\n")
-#             account_number = int(input("Enter your Account Number: "))
-#             pin = input("Enter your pin: ")
-#             print("Account Balance: $" + self.bank.check_balance(account_number, pin))
-#         except InvalidPinError as error:
-#             print(f"{error}")
-#             self.full_display(self.full_display_menu())
-#         except InvalidAccountNumberError as error:
-#             print(f"{error}")
-#             self.full_display(self.full_display_menu())
-#         except InvalidAmountError as error:
-#             print(f"{error}")
-#             self.full_display(self.full_display_menu())
-#         except AccountNotFoundError as error:
-#             print(f"{error}")
-#             self.full_display(self.full_display_menu())
-#         finally:
-#             self.full_display(self.full_display_menu())
-#
-#     def case_six(self):
-#         try:
-#             account_number = int(input("Enter the account number you want to remove: "))
-#             pin = input("Enter your pin: ")
-#             account = self.bank.find_account(account_number)
-#             if account.check_balance(account_number, pin) == 0:
-#                 choice = input(f"You still have {account.check_balance(account_number, pin)} in your balance\n"
-#                                f"1. continue to delete\n2. Main menu")
-#                 match choice:
-#                     case "1":
-#                         account_number = int(input("Enter the account number you want to remove: "))
-#                         pin = input("Enter your pin: ")
-#                         self.bank.remove_account(account_number, pin)
-#                     case 2:
-#                         self.full_display(3)
-#                     case _:
-#                         print("😒😒😒Wrong Input.\n\n")
-#                         self.case_six()
-#             self.bank.remove_account(account_number, pin)
-#             print("Account removed Successfully.\n\n")
-#         except InvalidPinError as error:
-#             print(f"{error}")
-#             self.case_six()
-#         except InvalidAccountNumberError as error:
-#             print(f"{error}")
-#             self.case_six()
-#         except InvalidAmountError as error:
-#             print(f"{error}")
-#             self.case_six()
-#         finally:
-#             self.full_display(self.full_display_menu())
-#
-#     def full_display(self, choice: int):
-#         match choice:
-#             case 1:
-#                 self.create_account()
-#             case 2:
-#                 self.case_two()
-#             case 3:
-#                 self.case_three()
-#             case 4:
-#                 self.case_four()
-#             case 5:
-#                 self.case_five()
-#             case 6:
-#                 self.case_six()
-#             case 7:
-#                 print("GoodBye😘😘😘")
-#                 sys.exit()
-#             case _:
-#                 print("🤦‍♂️🤦‍♂️🤦‍♂️🤦‍♂️\nWrong Input.")
-#                 self.full_display(self.full_display_menu())
-#
-#
 
 
 if __name__ == '__main__':
