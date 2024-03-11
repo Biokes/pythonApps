2482dab995b7ed571c6aee2399935159e6060e17
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-12 00:10:26 +0100

    my codes

--- games_arena/testtictactoe.py
+++ games_arena/testtictactoe.py
@@ -4,6 +4,12 @@ from apps.games_arena.tictactoe import TicTacToe
 
 class TestTictactoe:
 
-    def test_createNewGame_gameCellSAreEmpty(self):
+    def test_createNewGame_allGameCellSAreEmpty(self):
         tictactoe: TicTacToe = TicTacToe()
-        assert tictactoe.get_board_cell(1) == Cellvalues.EMPTY
+        for number in range(1, 10):
+            assert tictactoe.get_board_cell(number) == Cellvalues.EMPTY
+
+    def test_changeCellState_cellStateIsChanged(self):
+        tictactoe: TicTacToe = TicTacToe()
+        tictactoe.play(1)
+        assert tictactoe.get_board_cell(1) == Cellvalues.X
--- games_arena/tictactoe.py
+++ games_arena/tictactoe.py
@@ -1,9 +1,22 @@
+from apps.games_arena.cellvalue import Cellvalues
+
 class TicTacToe:
     def __init__(self):
-        self.board = [[], [], []]
+        self.board = [[Cellvalues.EMPTY, Cellvalues.EMPTY, Cellvalues.EMPTY],
+                      [Cellvalues.EMPTY, Cellvalues.EMPTY, Cellvalues.EMPTY],
+                      [Cellvalues.EMPTY, Cellvalues.EMPTY, Cellvalues.EMPTY]]
+        self.count = 0
 
     def get_board_cell(self, cell_number: int):
-        cell_number -= 1
-        row = cell_number // 3
-        column = cell_number % 3
+        row = (cell_number - 1) // 3
+        column = (cell_number - 1) % 3
         return self.board[row][column]
+
+    def play(self, cellNnumber: int):
+        row = (cellNnumber - 1) // 3
+        column = (cellNnumber - 1) % 3
+        if self.count % 2 == 0:
+            self.board[row][column] = Cellvalues.X
+        else:
+            self.board[row][column] = Cellvalues.O
+        self.count += 1
